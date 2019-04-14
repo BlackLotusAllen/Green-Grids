82545a35bf50f014b3044d8f00257f9af8a71152
BlackLotusAllen <274070102@qq.com>
2019-04-14 20:44:31 +0800

    谭浩强11.3

--- NEUOJ/main.c
+++ NEUOJ/main.c
@@ -2,36 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+int n;
+typedef struct student
 {
-    int year;
-    int mouth;
-    int day;
-}TValue;
-TValue t;
+    char id[100];
+    char name[100];
+    int score[3];
+}data;
+
+void input(data stu[])
+{
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", stu[i].id);
+        scanf("%s", stu[i].name);
+        for(int j = 0; j < 3; j++)
+        {
+            scanf("%d", &stu[i].score[j]);
+        }
+    }
+}
+
+void print(data stu[])
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%s", stu[i].id);
+        printf(",%s", stu[i].name);
+        for(int j = 0; j < 3; j++)
+        {
+            printf(",%d", stu[i].score[j]);
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
-    int mValue[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int runFlag, dValue;
-    while(scanf("%d %d %d", &t.year, &t.mouth, &t.day) != EOF)
+    while(scanf("%d", &n) != EOF)
     {
-        runFlag = 0;
-        dValue = 0;
-        if((t.year % 4 == 0 && t.year % 100 != 0) || (t.year % 400 == 0))
-        {
-            runFlag = 1;
-        }
-        for(int i = 0; i < t.mouth - 1; i++)
-        {
-            dValue = dValue + mValue[i];
-        }
-        dValue = dValue + t.day;
-        if(runFlag && t.mouth > 2)
-        {
-            dValue++;
-        }
-        printf("%d\n", dValue);
+        data stu[n];
+        input(stu);
+        print(stu);
     }
     return 0;
 }
