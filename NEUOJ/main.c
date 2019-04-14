1c3b4bf3689d244dd2b7d0758d139fe8592a209e
BlackLotusAllen <274070102@qq.com>
2019-04-14 20:45:53 +0800

    谭浩强11.8

--- NEUOJ/main.c
+++ NEUOJ/main.c
@@ -2,49 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-int n, best;
-int max[3];
-typedef struct student
+struct student
 {
-    char id[100];
-    char name[100];
-    int score[3];
-    int sum;
-}data;
-
-void input(data stu[])
-{
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%s", stu[i].id);
-        scanf("%s", stu[i].name);
-        stu[i].sum = 0;
-        for(int j = 0; j < 3; j++)
-        {
-            scanf("%d", &stu[i].score[j]);
-            stu[i].sum += stu[i].score[j];
-            max[j] += stu[i].score[j];
-        }
-    }
-}
+    int id;
+    int score;
+    struct student * next;
+};
 
 int main()
 {
-    while(scanf("%d", &n) != EOF)
-    {
-        data stu[n];
-        best = 0;
-        max[0] = 0, max[1] = 0, max[2] = 0;
-        input(stu);
-        printf("%d %d %d\n", max[0] / n, max[1] / n, max[2] / n);
+    int a, b, n, temp;
+    struct student * head = NULL;
+    struct student * previous, * current;
+    struct student * times, * min;
 
-        stu[best].sum = stu[0].sum;
-        for(int i = 1; i < n; i++)
+    while(scanf("%d %d", &a, &b) != EOF)
+    {
+        n = a + b;
+        for(int i = 0; i < n; i++)
         {
-            if(stu[best].sum < stu[i].sum)
-                best = i;
+            current = (struct student *)malloc(sizeof(struct student));
+            if(head == NULL)
+                head = current;
+            else
+                previous->next = current;
+            current->next = NULL;
+            scanf("%d %d", &current->id, &current->score);
+            previous = current;
         }
-        printf("%s %s %d %d %d\n", stu[best].id, stu[best].name, stu[best].score[0], stu[best].score[1], stu[best].score[2]);
+
+        times = head;
+        while(times->next != NULL)
+        {
+            min = times;
+            current = times->next;
+
+            while(current != NULL)
+            {
+                if(min->id > current->id)
+                    min = current;
+                current = current->next;
+            }
+
+            temp = times->id;
+            times->id = min->id;
+            min->id = temp;
+
+            temp = times->score;
+            times->score = min->score;
+            min->score = temp;
+
+            times = times->next;
+        }
+
+        current = head;
+        while(current != NULL)
+        {
+            printf("%d %d\n", current->id, current->score);
+            free(current);
+            current = current->next;
+        }
     }
     return 0;
 }
