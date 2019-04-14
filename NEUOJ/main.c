6253367cd469a96c0d610bcda2b4a8ee449e16bb
BlackLotusAllen <274070102@qq.com>
2019-04-14 20:43:52 +0800

    谭浩强11.1

--- NEUOJ/main.c
+++ NEUOJ/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int year;
+    int mouth;
+    int day;
+}TValue;
+TValue t;
 
 int main()
 {
-    printf("Hello world!\n");
+    int mValue[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int runFlag, dValue;
+    while(scanf("%d %d %d", &t.year, &t.mouth, &t.day) != EOF)
+    {
+        runFlag = 0;
+        dValue = 0;
+        if((t.year % 4 == 0 && t.year % 100 != 0) || (t.year % 400 == 0))
+        {
+            runFlag = 1;
+        }
+        for(int i = 0; i < t.mouth - 1; i++)
+        {
+            dValue = dValue + mValue[i];
+        }
+        dValue = dValue + t.day;
+        if(runFlag && t.mouth > 2)
+        {
+            dValue++;
+        }
+        printf("%d\n", dValue);
+    }
     return 0;
 }
